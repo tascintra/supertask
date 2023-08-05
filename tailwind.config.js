edd0404d65db820f1a128aacd17f440065994495
Tiago Cintra <tiago.slv@hotmail.com>
2023-08-05 17:48:00 -0300

    chore: design tokens configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          200: '#F9B384',
+          500: '#FC8879',
+          700: '#EE6F57',
+          900: '#F65050',
+        },
+        dark: {
+          300: '#2C2D36',
+          400: '#27282F',
+          500: '#1E1F23',
+          600: '#17181C',
+          700: '#111517',
+        },
+      },
+    },
   },
   plugins: [],
 }
