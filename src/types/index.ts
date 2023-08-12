4301ebc31a30a1e0324bcdd762eca87b75c2348f
Tiago Cintra <tiago.slv@hotmail.com>
2023-08-12 18:46:38 -0300

    refactor(types): types organized and centralized

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface TaskBase {
+  id: string
+  isCompleted: boolean
+  title: string
+}
+
+export type Subtask = TaskBase
+
+export interface Tasks extends TaskBase {
+  priority: 'high' | 'medium' | 'low'
+  progress: number
+  subtasks?: Subtask[]
+}
