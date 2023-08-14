e1f76c5d116633f75c0bb91e8dfbf9903ebc7cee
Tiago Cintra <tiago.slv@hotmail.com>
2023-08-14 16:21:14 -0300

    refactor(input): component refactored to receive forwardref

--- src/components/Molecules/Input/Input.tsx
+++ src/components/Molecules/Input/Input.tsx
@@ -1,34 +1,32 @@
-import clsx from 'clsx'
+import { forwardRef } from 'react'
 
 type InputProps = React.ComponentProps<'input'> & {
   inputLabel?: string
   type?: 'text' | 'date'
 }
 
-export const Input = ({
-  inputLabel,
-  className,
-  type,
-  ...inputProps
-}: InputProps) => {
-  return (
-    <div className={clsx(className)}>
-      <label
-        htmlFor={inputProps.name}
-        className={clsx('pl-4 text-sm font-semibold text-primary-900')}
-      >
-        {inputLabel}
-      </label>
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ inputLabel, className, type, ...inputProps }: InputProps, ref) => {
+    return (
+      <div className={className}>
+        <label
+          htmlFor={inputProps.name}
+          className="pl-4 text-sm font-semibold text-primary-900"
+        >
+          {inputLabel}
+        </label>
 
-      <input
-        type={type}
-        name={inputProps.name}
-        id={inputProps.name}
-        className={clsx(
-          'w-full overflow-hidden rounded-2xl border border-neutral-200 px-4 py-4 text-sm font-semibold leading-none shadow-component outline-none transition focus-within:shadow-focused dark:border-dark-400 dark:bg-dark-700 dark:text-neutral-200 dark:shadow-neutral-900 dark:focus-within:shadow-focused-dark',
-        )}
-        {...inputProps}
-      />
-    </div>
-  )
-}
+        <input
+          type={type}
+          name={inputProps.name}
+          id={inputProps.name}
+          ref={ref}
+          className="w-full overflow-hidden rounded-2xl border border-neutral-200 px-4 py-4 text-sm font-semibold leading-none shadow-component outline-none transition focus-within:shadow-focused dark:border-dark-400 dark:bg-dark-700 dark:text-neutral-200 dark:shadow-neutral-900 dark:focus-within:shadow-focused-dark"
+          {...inputProps}
+        />
+      </div>
+    )
+  },
+)
+
+Input.displayName = 'Input'
